24f4702ae2992200fdf4e5615bdde8128e455b32
Garv <garvchhokra@gmail.com>
2022-10-24 23:17:59 -0400

    first

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //local MongoDB deployment ->
+    // "URI": "mongodb://localhost/books229"
+    // URI: "mongodb://127.0.0.1:27017/books229",
+    URI: "mongodb+srv://book:book@bookdbserver.wyzz9fy.mongodb.net/books?retryWrites=true&w=majority",
+};
